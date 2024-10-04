3c7ef204d3914de3a8c2b014302ff3e833ebdd2d
aditiakurniawan <aditiakurniawanx@gmail.com>
2024-10-04 14:55:38 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\Halo\HaloController;
+use App\Http\Controllers\Todo\TodoController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/halo',[HaloController::class, 'index']);
+
+Route::get('/todo', [TodoController::class, 'index']);
+Route::post('/todo', [TodoController::class, 'store']);
